64fd47b03dec1d57dacd599b64cd66da18b07b7f
Unknown <raquelpa93@gmail.com>
2017-10-02 12:26:51 +0200

    A lot of stuff

---
+++ AES/GF.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+def int_to_bin (a):
+    elembin = bin(a)[2:len(bin(a))]
+    return list(map(int, elembin))
+
+
+def GF_product_p(a, b):
+    prod = np.polymul(a, b)
+    res =
+
+def GF_tables():
+    pass
+
+def GF_product_t(a,b):
+    pass
+
+def GF_generator():
+    pass
+
+def GF_invers(a):
+    pass
