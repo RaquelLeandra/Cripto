7fcc426374dfe9ec80596ddf9319d380b4182d39
Unknown <raquelpa93@gmail.com>
2017-11-28 18:43:33 +0100

    añado curvas elipticas

---
+++ ECC/ecc.py
@@ -0,0 +1,9 @@
+
+
+firmar='47ecc5be5c9d09ac308c0f30c77c71c0fdcfb080c8a29b7881fb1547336587f7c50fd266544ed89f52bb959d99f2d514756a91f5c6d28bbba79babce25d38936'
+
+print(firmar[:int(64)])
+print(len(firmar[:int(64)]))
+print(len(firmar))
+
+firma = 0x47ecc5be5c9d09ac308c0f30c77c71c0fdcfb080c8a29b7881fb1547336587f7
